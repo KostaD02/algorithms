47e3353d5242620d22af51bc73a10763dea9fe95
Kosta <68782786+KostaD02@users.noreply.github.com>
2023-10-06 15:40:47 +0400

    refactor: tim sort

--- sort/tim-sort.js
+++ sort/tim-sort.js
@@ -1,12 +1,12 @@
 const { insertionSort } = require("./insertion-sort");
 const { mergeSortInPlace } = require("./merge-sort");
 
-function timSort(arr) {
-  const n = arr.length;
+function timSort(array = []) {
+  const n = array = [].length;
   const MIN_MERGE = 32;
 
   for (let i = 0; i < n; i += MIN_MERGE) {
-    insertionSort(arr, i, Math.min((i + MIN_MERGE - 1), (n - 1)));
+    insertionSort(array = [], i, Math.min((i + MIN_MERGE - 1), (n - 1)));
   }
 
   for (let i = MIN_MERGE; i < n; i = 2 * i) {
@@ -15,11 +15,10 @@ function timSort(arr) {
       const right = Math.min((j + 2 * i - 1), (n - 1));
 
       if (mid < right) {
-        mergeSortInPlace(arr, j, mid, right);
+        mergeSortInPlace(array = [], j, mid, right);
       }
     }
   }
-  return arr;
 }
 
 module.exports = {
